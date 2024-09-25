4ea470025968f9efd4b42c6e64318519c373e1e7
CHW_1207 <165626148+dilkusha27@users.noreply.github.com>
2024-09-25 11:28:04 +0900

    PR sample (#8)
    * 로그인 페이지 구현
    
    * 병합 전 커밋
    
    * 회원가입 구현 진행 중
    
    * 회원가입 유효성 검사 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import LoginPage from "./appmain/LoginPage";
+import LoginPage2 from "./appmain/LoginPage2";
+import SignupPage from "./appmain/SignupPage";
 
 function App() {
     return (
@@ -8,6 +10,8 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="login" element={<LoginPage />} />
+                    <Route path="login2" element={<LoginPage2 />} />
+                    <Route path="signup" element={<SignupPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
