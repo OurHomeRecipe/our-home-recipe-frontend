d18c9369b1a3c0ef97a6961dbeb3dc7149e4e522
Soyeon <isylsy166@gmail.com>
2024-09-29 21:25:31 +0900

    Feature/join (#14)
    * 회원가입
    
    * 회원가입

--- src/pages/joinPage/JoinPage.presenter.jsx
+++ src/pages/joinPage/JoinPage.presenter.jsx
@@ -3,10 +3,21 @@ import page from '../../css/pages/page.common.module.css'
 import style from '../../css/pages/joinpage.module.css'
 import PostEmailAuth from '../../api/queries/post.emailAuth'
 import GetEmailAuth from '../../api/queries/get.emailAuth'
+import PostMemberRegister from '../../api/queries/post.member.register'
 
 export default function JoinPageUI({
   email,
   authCode,
+  nickname,
+  password,
+  phoneNumber,
+  userName,
+  setNickName,
+  setPassword,
+  passwordConfirm,
+  setPasswordConfirm,
+  setPhoneNumber,
+  setUserName,
   setEmail,
   setAuthCode
 }) {
@@ -32,30 +43,37 @@ export default function JoinPageUI({
 
           <div className={style.joinBox}>
             <p>닉네임</p>
-            <input type='text' placeholder='최소 2글차 최대 12글자'/>
+            <input type='text' placeholder='최소 2글차 최대 12글자' onChange={(e) => setNickName(e.target.value)}/>
           </div>
 
           <div className={style.joinBox}>
             <p>연락처</p>
-            <input type='text' placeholder='연락처 형식에 맞게 작성해주세요'/>
+            <input type='text' placeholder='연락처 형식에 맞게 작성해주세요' onChange={(e) => setPhoneNumber(e.target.value)}/>
           </div>
 
           <div className={style.joinBox}>
             <p>이름</p>
-            <input type='text' placeholder='이름을 입력해주세요'/>
+            <input type='text' placeholder='이름을 입력해주세요' onChange={(e) => setUserName(e.target.value)}/>
           </div>
 
           <div className={style.joinBox}>
             <p>비밀번호</p>
-            <input type='password' placeholder='최소 8글자, 영문, 숫자를 포함해주세요'/>
+            <input type='password' placeholder='최소 8글자, 영문, 특수문자, 숫자를 포함해주세요' onChange={(e) => setPassword(e.target.value)}/>
           </div>
 
           <div className={style.joinBox}>
             <p>비밀번호 확인</p>
-            <input type='password' placeholder='연락처 형식에 맞게 작성해주세요'/>
+            <input type='password' placeholder='비밀번호를 다시 입력해주세요' onChange={(e) => setPasswordConfirm(e.target.value)}/>
           </div>
 
-          <button type='button' className={style.joinButton}>회원가입</button>
+          <PostMemberRegister
+            email={email}
+            nickname={nickname}
+            phoneNumber={phoneNumber}
+            name={userName}
+            password={password}
+            passwordConfirm={passwordConfirm}
+          />
 
         </div>
     </div>
