24f4d1f32b43c2329ede074137e516ee04e29f52
Soyeon <isylsy166@gmail.com>
2024-09-29 20:00:11 +0900

    회원가입

--- src/pages/joinPage/JoinPage.container.jsx
+++ src/pages/joinPage/JoinPage.container.jsx
@@ -5,13 +5,28 @@ export default function JoinPage() {
 
   const [email, setEmail] = useState('');
   const [authCode, setAuthCode] = useState('');
+  const [nickname, setNickName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [userName, setUserName] = useState();
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState();
+
 
 
   return (
     <JoinPageUI
       email={email}
       authCode={authCode}
+      nickname={nickname}
+      phoneNumber={phoneNumber}
+      userName={userName}
+      password={password}
+      passwordConfirm={passwordConfirm}
+      setNickName={setNickName}
+      setPassword={setPassword}
+      setPasswordConfirm={setPasswordConfirm}
+      setPhoneNumber={setPhoneNumber}
+      setUserName={setUserName}
       setEmail={setEmail}
       setAuthCode={setAuthCode}
     />
