f3ce02b092b6d2b02f2ce49711e71eee35f98116
elrdan <seongo9655@gmail.com>
2024-10-05 05:13:14 +0900

    feat: 프로필 업데이트에 필요한 부분 추가했습니다.

--- src/api/axios/post.me.profile.js
+++ src/api/axios/post.me.profile.js
@@ -4,12 +4,17 @@ import API from "../interceptor/API";
 
 export const updateProfile = async (profileData) => {
     try {
-      const  response  = await API.post(
-        '/member/me/profile',
-        profileData,
-        {withCredentials: true}
-    );
-      return response.data;
+        const response = await API.post(
+            '/member/me/profile',
+            profileData,
+            {
+                headers: {
+                    "Content-Type": 'multipart/from-data'
+                }
+            },
+            {withCredentials: true}
+        );
+        return response.data;
     } catch (error) {
         console.error('Error fetching profile:', error);
         console.log(error.errorMessage);
