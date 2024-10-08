0770dfb165632b57da2f180c5a1781636d80878a
Soyeon <isylsy166@gmail.com>
2024-10-08 13:48:45 +0900

    레시피 등록 컴포넌트 분리

--- src/pages/mypage/addRecipePage/addRecipePage.jsx
+++ src/pages/mypage/addRecipePage/addRecipePage.jsx
@@ -2,46 +2,21 @@
 import { useRecipeMetaDataQuery } from '../../../api/queries/recipeQueries';
 import page from '../../../css/pages/page.common.module.css'
 import style from './style/addrecipepage.module.css'
-import { LuImagePlus } from "react-icons/lu";
-import { FoodImg } from './style/addRecipe.Page.style';
-import useImageUpload from '../../../common/hook/useImageUpload';
 import RecipeIngredients from './component/ingredients';
 import RecipeTags from './component/tags';
+import RecipeInfo from './component/recipeInfo';
 
 export default function AddRecipePage() {
 
-    //이미지 업로드
-    const {preview, fileInputRef, handleIconClick, handleImageChange } = useImageUpload();
-
     //재료, 태그정보 가져오기
     const {ingredients, tags} = useRecipeMetaDataQuery();
-
-    
-
+  
     return (
       <div className={page.frame}>
           <h1>레시피 등록</h1>
-  
-          <div className={style.foodInfo}>
-              <div className={style.foodInfo_left}>
-                  <div className={style.inputBox}>
-                      <p>레시피 제목</p>
-                      <input type='text' placeholder='예) 진짜 엽떡맛 나는 떡볶이'/>
-                  </div>
-  
-                  <div className={style.inputBox}>
-                      <p>요리 소개</p>
-                      <textarea type='text' placeholder='이 레시피의 탄생 배경을 적어주세요. 예) 엽떡을 자주 시켜먹어서 직접 만들어 본 레시피 입니다.'/>
-                  </div>        
-              </div>
-  
-              <FoodImg preview={preview} onClick={handleIconClick}>
-                  {preview === null ? <LuImagePlus color='gray' size={25} /> : ''}               
-                  <input ref={fileInputRef} type="file" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} />
-              </FoodImg>
-          </div>
+            <RecipeInfo/>
 
-          <RecipeTags tags={tags}/>
+            <RecipeTags tags={tags}/>
   
           <hr/>
   
--- src/pages/mypage/addRecipePage/component/tags.jsx
+++ src/pages/mypage/addRecipePage/component/tags.jsx
@@ -2,20 +2,56 @@ import React from 'react'
 import style from '../style/addrecipepage.module.css'
 
 export default function RecipeTags({tags}) {
+
   return (
     <div className={style.foodInfo}>
     <div className={style.tagBox}>
-        {tags.map((tag,index) => 
-            <div key={index} className={style.selectBox}>
-                <p>{tag.tagTypeName}:</p>
-                <select>
-                    <option value='' >선택</option>
-                    {tag.tags.map((tag) =>
-                        <option key={tag.tagId} value={tag.tagId} >{tag.tagName}</option>
-                    )}
-                </select>
-            </div> 
-        )}                          
+
+        {/* 식사시간 */}
+        <div className={style.selectBox}>
+            <p>{tags[0]?.tagTypeName}</p>
+            <select >
+                <option>선택</option>
+                {tags[0]?.tags.map(tag => 
+                    <option key={tag.tagId} value={tag.tagId}>{tag.tagName}</option>
+                )}
+            </select>                          
+        </div>
+
+        {/* 계절 */}
+        <div className={style.selectBox}>
+            <p>{tags[1]?.tagTypeName}</p>
+            <select >
+                <option>선택</option>
+                {tags[1]?.tags.map(tag => 
+                    <option key={tag.tagId} value={tag.tagId}>{tag.tagName}</option>
+                )}
+            </select>                          
+        </div>
+        
+        {/* 인분 */}
+        <div className={style.selectBox}>
+            <p>{tags[2]?.tagTypeName}</p>
+            <select >
+                <option>선택</option>
+                {tags[2]?.tags.map(tag => 
+                    <option key={tag.tagId} value={tag.tagId}>{tag.tagName}</option>
+                )}
+            </select>                          
+        </div>
+
+        {/* 목적 */}
+        <div className={style.selectBox}>
+            <p>{tags[3]?.tagTypeName}</p>
+            <select >
+                <option>선택</option>
+                {tags[3]?.tags.map(tag => 
+                    <option key={tag.tagId} value={tag.tagId}>{tag.tagName}</option>
+                )}
+            </select>                          
+        </div>
+
+
     </div>
 </div>
   )
