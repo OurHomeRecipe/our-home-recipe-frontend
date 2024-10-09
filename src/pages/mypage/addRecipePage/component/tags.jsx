f4d1ba8495667a7d590578acdf416de85cfba209
elrdan <seongo9655@gmail.com>
2024-10-09 22:43:34 +0900

    fix: getSelectOption 처리하는 방식 수정

--- src/pages/mypage/addRecipePage/component/tags.jsx
+++ src/pages/mypage/addRecipePage/component/tags.jsx
@@ -1,17 +1,16 @@
 import React, {useEffect} from 'react'
 import style from '../style/addrecipepage.module.css'
 
-
 export default function RecipeTags({tags, setRecipeData}) {
 
-    const getSelectOption = (e) => {
+    const getSelectOption = (e, tagTypeName) => {
         const tagId = e.target.value;
         const tagName = e.target.options[e.target.selectedIndex].textContent;
         setRecipeData(prev => ({
             ...prev,
             tags: [
                 ...prev.tags,
-                {tagId: tagId, tagName: tagName}
+                {tagId, tagName, tagTypeName}
             ]
         }));
     }
@@ -23,7 +22,7 @@ export default function RecipeTags({tags, setRecipeData}) {
                     <div key={index} className={style.selectBox}>
                         <p>{tagGroup.tagTypeName}</p>
 
-                        <select name={`select${index}`} onChange={getSelectOption}>
+                        <select name={`select${index}`} onChange={(e) => getSelectOption(e, tagGroup?.tagTypeName)}>
                             <option value=''>선택</option>
 
                             {tagGroup.tags.map(tag =>
