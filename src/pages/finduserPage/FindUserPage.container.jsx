4b6348c430b9f240c74dc272b6b5df50e0738092
elrdan <163220957+elrdan@users.noreply.github.com>
2024-10-10 06:23:16 +0900

    feat: 비밀번호 찾기(초기화) UI 및 기능 추가 (#40)

--- src/pages/finduserPage/FindUserPage.container.jsx
+++ src/pages/finduserPage/FindUserPage.container.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FindUserUI from './FindUserPage.presenter'
+import {useFindUserResetPassword} from "../../api/queries/findUserQuery";
 
 export default function FindUserPage() {
-  return (
-    <FindUserUI/>
-  )
+    const {handleResetPassword} = useFindUserResetPassword();
+    const [data, setData] = useState({
+        email: '',
+        name: '',
+        phoneNumber: ''
+    });
+
+
+    // 유효성 검사 메서드
+    const validateResetPasswordInputs = () => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!emailRegex.test(data.email)) {
+            alert("유효한 이메일을 입력해주세요.");
+            return false;
+        }
+
+        const nameRegex = /^[가-힣a-zA-Z]{2,20}$/;
+        if (!nameRegex.test(data.name)) {
+            alert("유효한 이름을 입력해주세요.");
+            return false;
+        }
+
+        const phonNumberRegex = /^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/;
+        if (!phonNumberRegex.test(data.phoneNumber)) {
+            alert("유효한 핸드폰 번호를 입력해주세요.");
+            return false;
+        }
+
+        return true;
+    }
+
+    // 비밀번호 찾기 버튼 클릭 시 실행되는 함수
+    const resetPassword = () => {
+        if (validateResetPasswordInputs()) {
+            handleResetPassword(data);
+        }
+    }
+
+    return (
+        <FindUserUI
+            data={data}
+            setData={setData}
+            resetPassword={resetPassword}
+        />
+    )
 }
--- src/pages/finduserPage/FindUserPage.presenter.jsx
+++ src/pages/finduserPage/FindUserPage.presenter.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import {InputBox} from "./FindUserPage.style";
+import style from '../../css/pages/finduserpage.module.css'
 
-export default function FindUserUI() {
+export default function FindUserUI({data, setData, resetPassword}) {
   return (
-    <div>FindUserUI</div>
+      <div className={style.frame}>
+          <h1>비밀번호 찾기</h1>
+
+          <div className={style.row}>
+              <p>이메일: </p>
+              <InputBox
+                  placeholder={'example@test.com'}
+                  value={data.email}
+                  onChange={(e) => setData(prev => ({
+                      ...prev,
+                      email: e.target.value
+                  }))}
+              />
+          </div>
+
+          <div className={style.row}>
+              <p>이름: </p>
+              <InputBox
+                  placeholder={'테스트'}
+                  value={data.name}
+                  onChange={(e) => setData(prev => ({
+                      ...prev,
+                      name: e.target.value
+                  }))}
+              />
+          </div>
+
+          <div className={style.row}>
+              <p>핸드폰 번호: </p>
+              <InputBox
+                  placeholder={'010-1234-5678'}
+                  value={data.phoneNumber}
+                  onChange={(e) => setData(prev => ({
+                      ...prev,
+                      phoneNumber: e.target.value
+                  }))}
+              />
+          </div>
+
+          <button className={style.button} onClick={resetPassword}>비밀번호 찾기</button>
+      </div>
   )
 }
