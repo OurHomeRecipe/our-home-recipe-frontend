781be4411996ca8e0f0014b2f8287ea5cd5c5165
Soyeon <isylsy166@gmail.com>
2024-10-12 07:07:08 +0900

    레시피 상세페이지 디자인 수정

--- src/pages/recipe/detailRecipePage/detailRecipePage.jsx
+++ src/pages/recipe/detailRecipePage/detailRecipePage.jsx
@@ -9,8 +9,21 @@ export default function DetailRecipePage() {
     const recipeId = location.pathname.split('/')[2]; //레시피 아이디
     console.log('레시피id:', recipeId);
 
-    const {member, recipeImage, recipeName, recipeDescription, tags, ingredients} = useRecipeDetailQuery(recipeId);
+    const { data, error, isLoading } = useRecipeDetailQuery(recipeId);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    const { member, recipeImage, recipeName, recipeDescription, tags, ingredients } = data || {};
+
     console.log('회원', member);
+
+    
 
   return (
     <div className={common.frame}>
@@ -22,8 +35,30 @@ export default function DetailRecipePage() {
             <p>{member.memberNickname}</p>
         </div>
 
-        <div>
+        <div className={style.contentBox}>
             <h1>{recipeName}</h1>
+            <pre>{recipeDescription}</pre>
+
+            <hr/>
+
+            {tags.map((tag, index) => 
+                <div key={index} className={style.tagBox}>
+                    <p>{tag.tagTypeName} :</p>
+                    <p>{tag.tags[0].tagName}</p>
+                </div>
+            )}
+
+            <hr/>
+
+            <h2>재료</h2>
+            {ingredients.map( ingredient => 
+                <div key={ingredient.ingredientId} className={style.ingredientBox}>
+                    <p>{ingredient.ingredientName}</p>
+                    <p>{ingredient.ingredientQuantity}</p>
+                    <p>{ingredient.ingredientUnit}</p>
+                </div>
+            )}
+
         </div>
     </div>
   )
