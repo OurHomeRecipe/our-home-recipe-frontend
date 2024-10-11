d6f856219ef2c2aa890bbb788d23299b3012e941
Soyeon <isylsy166@gmail.com>
2024-10-11 21:26:45 +0900

    재료등록 수정 (#41)

--- src/common/component/table/style/basicTable.style.js
+++ src/common/component/table/style/basicTable.style.js
@@ -4,10 +4,12 @@ export const PreviewImg = styled.div`
     background: url(${(props) => props.preview}) no-repeat;
     background-size: 100%;
     display: flex;
-    width: 178px;
-    height: 159px;
+    width: 300px;
+    height: 180px;
     border-radius: 10px;
     background-color: #f1f1f1;
     align-items: center;
     justify-content: center;
+    margin: 16px 0px;
 `;
+
